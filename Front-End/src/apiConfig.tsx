307247181de8ef7139a7875922e2de4f64a96fcc
Channa Dissanayaka <ywchanna@gmail.com>
2024-09-22 19:16:29 +0530

    modified

--- Front-End/src/apiConfig.tsx
+++ Front-End/src/apiConfig.tsx
@@ -7,4 +7,9 @@ export const API_ENDPOINTS = {
     PREDICT: `${BASE_URL}/predict`,
     FEEDBACK: `${BASE_URL}/feedback`,
     MEDICAL_CENTERS: `${BASE_URL}/medicalcenters`,
+
+     // New API endpoints for predictions
+     PREDICT_DOSHA: `${BASE_URL}/predict/dosha`,
+     PREDICT_RISK: `${BASE_URL}/predict/risk`,
+     PREDICT_MEDICINE: `${BASE_URL}/predict/medicine`,
 };
